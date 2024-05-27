11dbade33b92d1463e8707477503b92de436e725
Gayatrinarule <gayatri.n@pramanindia.in>
2024-05-27 17:36:11 +0530

    add

---
+++ BaseClass.java
@@ -0,0 +1,11 @@
+package advanced.accessmodifiers;
+
+public class BaseClass {
+   
+    private int baseVariable = 10;
+    
+  
+    protected void baseMethod() {
+        System.out.println("BaseClass: baseMethod()");
+    }
+}
